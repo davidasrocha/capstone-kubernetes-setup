da65f164de57a4c70f2b48ec98c8601337c7d735
David Anderson <david.anderson.sil@gmail.com>
2019-10-05 12:58:37 -0300

    chore: Add wait operation to stck delete
    eksctl start the cluster delete, but skip with success before stack be
    deleted.

--- delete.sh
+++ delete.sh
@@ -10,3 +10,10 @@ CLUSTER_NAME="$1"
 
 eksctl delete cluster \
     --name "$CLUSTER_NAME"
+
+echo ""
+echo "Waiting stack eksctl-$CLUSTER_NAME-cluster delete"
+echo ""
+
+aws cloudformation wait stack-delete-complete \
+    --stack-name "eksctl-$CLUSTER_NAME-cluster"
