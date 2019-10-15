4257208076f260e951b73e75fa9e7c4e5870c891
David Anderson <david.anderson.sil@gmail.com>
2019-10-15 19:22:43 -0300

    chore: Improve json parse to AWS Secrets Manager

--- devops_kubernetes_secret_docker_hub.sh
+++ devops_kubernetes_secret_docker_hub.sh
@@ -3,10 +3,10 @@
 SECRET_NAME="$1"
 SECRET_ID="$2"
 
-SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ID" --output json)
+SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ID" --query SecretString --output json)
 
-PRIVATE_DOCKER_USERNAME=$(echo $SECRET_VALUE | jq -r '.SecretString | fromjson | .PRIVATE_DOCKER_USERNAME')
-PRIVATE_DOCKER_PASSWORD=$(echo $SECRET_VALUE | jq -r '.SecretString | fromjson | .PRIVATE_DOCKER_PASSWORD')
-PRIVATE_DOCKER_EMAIL=$(echo $SECRET_VALUE | jq -r '.SecretString | fromjson | .PRIVATE_DOCKER_EMAIL')
+PRIVATE_DOCKER_USERNAME=$(echo $SECRET_VALUE | jq -r '. fromjson | .PRIVATE_DOCKER_USERNAME')
+PRIVATE_DOCKER_PASSWORD=$(echo $SECRET_VALUE | jq -r '. fromjson | .PRIVATE_DOCKER_PASSWORD')
+PRIVATE_DOCKER_EMAIL=$(echo $SECRET_VALUE | jq -r '. fromjson | .PRIVATE_DOCKER_EMAIL')
 
 kubectl create secret docker-registry "$SECRET_NAME-private-registry" "--docker-username=$PRIVATE_DOCKER_USERNAME" "--docker-password=$PRIVATE_DOCKER_PASSWORD" "--docker-email=$PRIVATE_DOCKER_EMAIL"
